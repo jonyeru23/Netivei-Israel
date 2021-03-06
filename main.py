6b327a6f7f32327253d12acf8ebfe0b3c59eb613
Jonathan Yerushalmi <71553438+jonyeru23@users.noreply.github.com>
2021-03-06 23:40:32 +0200

    Update main.py
    not finished!!

--- main.py
+++ main.py
@@ -1,26 +1,10 @@
-from urllib import request
-from bs4 import BeautifulSoup as bs
+from page import FrontPage
 
 def main():
     # building my front page
-    # page = FrontPage()
-    html = request.urlopen("https://www.iroads.co.il/").read()
-    soup = bs(html, features="lxml")
-    # print(soup)
-
-    # filter
-    filter = "מידע-לספקים"
-    tags = soup('a')
-    for tag in tags:
-        link = tag.get('href', None)
-        if link is None:
-            continue
-        if filter in link:
-            print(tag.figcaption.string)
-        # the_link = tag.get('href', None)
-        # if the_link is not None and the_link.find(filter) != -1:
-        #     print(tag.get('figcaption', None))
-        # print(tag)
+    page = FrontPage()
+    page.show_subs()
+    page.root.mainloop()
 if __name__ == '__main__':
     main()
 
