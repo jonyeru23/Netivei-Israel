ab07637ae3b1a47115f5ba2ebe8cb6b736a32d29
Jonathan Yerushalmi <71553438+jonyeru23@users.noreply.github.com>
2021-03-08 00:10:16 +0200

    Update helper.py

--- helper.py
+++ helper.py
@@ -3,13 +3,13 @@ from tkinter import *
 
 
 
-def show_subs(page, options):
-    page.subs_clicked.set('לא נבחר')
-    drop = OptionMenu(page.root, page.subs_clicked, *options).pack()
-    myButton = Button(page.root, text='submit', command=answer).pack()
-
-
-def answer():
-    picked = Label(self.root, text=subs_clicked.get()).pack()
-    # return self.subs_clicked.get()
-
+# def show_subs(page, options):
+#     page.subs_clicked.set('לא נבחר')
+#     drop = OptionMenu(page.root, page.subs_clicked, *options).pack()
+#     myButton = Button(page.root, text='submit', command=answer).pack()
+#
+#
+# def answer():
+#     picked = Label(self.root, text=subs_clicked.get()).pack()
+#     # return self.subs_clicked.get()
+#
