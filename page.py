397aeb59399f1ac7b0139700f09a80ccd5625552
Jonathan Yerushalmi <71553438+jonyeru23@users.noreply.github.com>
2021-03-08 00:09:43 +0200

    Update page.py
    yes

--- page.py
+++ page.py
@@ -2,70 +2,124 @@ from tkinter import *
 from urllib import request
 import helper
 from bs4 import BeautifulSoup as bs
+import requests
+
 
 # THE MAIN URL
 url_main = "https://www.iroads.co.il/"
+filter = 'מידע-לספקים'
 
 
 class FrontPage():
     """
     this class is for building the basic basic front
     """
+
     def __init__(self):
         self.root = Tk()
         self.subject = Label(self.root, text=":מה הנושא המרכזי").pack()
-        self.subs_clicked = StringVar()
-    # build the screen
+        # build the screen
         self.root.title("למצוא את התקן")
         self.root.geometry("400x400")
 
-    # get a text bar with options
-    # def show_subs(self):
-    #
-    #
-    # def answer(self):
-    #     picked = Label(self.root, text=self.subs_clicked.get()).pack()
-    #     # return self.subs_clicked.get()
 
-class ReadSubjects():
+def ReadSubjects():
     """
-    this class is for reading the prime subjects
+    get me the subjects and return two datasets:
+    1. a list of options
+    2. a dict of option - href
     """
-    def __init__(self, the_filter, url):
-        self.the_filter = the_filter
-        self.url = url
-        self.options = self.add_subs()
+    hrefs = {}
+    html = requests.get(url_main)
+    soup = bs(html.text, features="lxml")
 
-    def add_subs(self):
-        """
-        go to the 
-        """
-        options = []
-        html = request.urlopen(self.url).read()
-        soup = bs(html, features="lxml")
+    tags = soup.findAll('a', class_='infoIcon')
 
-        # filter
-        tags = soup('a')
-        for tag in tags:
-            link = tag.get('href', None)
-            if link is None:
-                continue
-            if self.the_filter in link:
-                options.append(tag.figcaption.string)
-        return options
+    # filter
+    for tag in tags:
+        link = tag.get('href', None)
+        if link is None:
+            continue
+        if filter in link:
+            option = tag.figcaption.string
+            # adding to my dict the links
+            hrefs[option] = link
 
-class Display_Subjects():
+    return hrefs
+
+class Display():
+    def __init__(self, root):
+        self.root = root
+        self.clicked = StringVar()
+
+
+class Display_Subjects(Display):
     """
     this class is for displaying options menus
     """
-    def __init__(self, options, root):
-        self.root = root
+
+    def __init__(self, options, root, hrefs):
+        super().__init__(root)
         self.options = options
-        self.clicked = StringVar()
+        self.hrefs = hrefs
 
         # building a options menu
         self.clicked.set('לא נבחר')
-        drop = OptionMenu(self.root, self.clicked, *self.options).pack()
-        # myButton = Button(self.root, text='submit', command=self.answer).pack()
+        # print(self.options)
+        self.drop = OptionMenu(self.root, self.clicked, *self.options).pack()
+        mybutton = Button(self.root, text='submit', command=self.change).pack()
 
-    
+
+    def change(self):
+        """
+        this is where i'm going to set the new options
+        """
+        sub_subs = ReadSubSubjects(self.hrefs, self.clicked.get())
+        sub_subs.get_headers()
+
+        sub_menu = DisplaySubSubjects(sub_subs.options, self.root)
+
+
+class ReadSubSubjects():
+    """
+    this class is for getting the headers of the page we have chosen to enter
+    """
+    def __init__(self, hrefs, clicked):
+        self.hrefs = hrefs
+        self.options = ['תבחר נושא']
+        self.clicked = str(clicked)
+
+    def get_headers(self):
+        # print(self.clicked)
+        if self.clicked not in self.hrefs.keys():
+            return
+        self.options.clear()
+        # making out way for the page
+        url = requests.get(url_main, self.hrefs[self.clicked][1:-1])
+        subject_url = requests.get(url.url.replace('?', ''))
+        # get the soup
+        soup = bs(subject_url.text, features="lxml")
+
+        # get the relevant headers
+        bigs = soup.findAll('div', class_='container-fluid col9')
+
+        for big in bigs:
+            self.options.append(big.article.h2.text)
+
+
+class DisplaySubSubjects(Display):
+    def __init__(self, options, root):
+        super().__init__(root)
+        self.options = options
+
+        # build the interface
+        self.drop = OptionMenu(self.root, self.clicked, *self.options).pack()
+        mybutton = Button(self.root, text='submit', command=self.ask).pack()
+
+    def ask(self):
+        """
+        asking the user what he/she would like to know
+        """
+        pass
+
+
