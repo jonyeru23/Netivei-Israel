98a2569db58307f38607f28585c711d7baf2dd53
Jonathan Yerushalmi <71553438+jonyeru23@users.noreply.github.com>
2021-03-07 16:58:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from page import FrontPage
+from page import *
+
+url_main = "https://www.iroads.co.il/"
+
 
 def main():
     # building my front page
     page = FrontPage()
-    page.show_subs()
+    # build a option menu
+    filter = 'מידע-לספקים'
+    main_subs = ReadSubjects(filter, url_main)
+    main_menu = Display_Subjects(main_subs.options, page.root)
     page.root.mainloop()
+
+
 if __name__ == '__main__':
     main()
 
