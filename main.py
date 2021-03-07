a15fea1b6544d4bc8b1f8ba1b71b3117ebd4b213
Jonathan Yerushalmi <71553438+jonyeru23@users.noreply.github.com>
2021-03-08 00:09:05 +0200

    Update main.py
    progress

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from page import *
+from urllib import request, parse
+import requests
 
 url_main = "https://www.iroads.co.il/"
 
@@ -6,11 +8,20 @@ url_main = "https://www.iroads.co.il/"
 def main():
     # building my front page
     page = FrontPage()
-    # build a option menu
-    filter = 'מידע-לספקים'
-    main_subs = ReadSubjects(filter, url_main)
-    main_menu = Display_Subjects(main_subs.options, page.root)
+
+    # getting the subjects from the web
+    main_subs_links = ReadSubjects()
+    # making the dropdown
+    main_menu = Display_Subjects(main_subs_links.keys(), page.root, main_subs_links)
+
+
+    # # making the dropdown
+    # sub_menu = Display_Subjects(sub_subs.options, page.root)
+
     page.root.mainloop()
+
+    """ checking things"""
+
 
 
 if __name__ == '__main__':
