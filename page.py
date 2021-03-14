529dc52c5229430c0fcce30bfefdaecabfe210e3
Jonathan Yerushalmi <71553438+jonyeru23@users.noreply.github.com>
2021-03-14 23:25:30 +0200

    Update page.py

--- page.py
+++ page.py
@@ -1,9 +1,5 @@
 from tkinter import *
 from helper import *
-from urllib import request
-import helper
-from bs4 import BeautifulSoup as bs
-import requests
 
 """
 this is a global list of all the display menus
@@ -49,6 +45,9 @@ class DisplayMenu():
 
 
 class MyButton():
+    """
+    the class for the buttons of the program
+    """
     def __init__(self, menu, sub_links, url):
         self.sub_links = sub_links
         self.url = url
@@ -56,10 +55,15 @@ class MyButton():
         self.button = Button(self.menu.root, text="!בחרתי", command=self.get)
 
     def get(self):
+        """
+        if it is the first option menu create the second one
+        if it's the second, ask the question and make the magic happen
+        """
 
         if self.menu.first:
             if len(display_menus) > 1:
-                display_menus[-1].remove()
+                for i in range(1, len(display_menus)):
+                    display_menus[i].remove()
             # find the subs -- function
             sub_subs_links, subject_url = get_headers(str(self.menu.clicked.get()), self.sub_links, self.url)
 
@@ -73,72 +77,23 @@ class MyButton():
             """
             ask the fucking question
             """
-            print("fuck")
-
-# class Display():
-#     def __init__(self, root):
-#         self.root = root
-#         self.clicked = StringVar()
-#
-#
-# class Display_Subjects(Display):
-#     """
-#     this class is for displaying options menus
-#     """
-#
-#     def __init__(self, options, root, hrefs):
-#         super().__init__(root)
-#         self.options = options
-#         self.hrefs = hrefs
-#
-#         # building a options menu
-#         self.clicked.set('לא נבחר')
-#         self.make_options()
-#
-#     def make_options(self):
-#         """
-#         building a label a menu and a button
-#         """
-#         main_label = Label(self.root, text=":מה הנושא המרכזי")
-#         main_label.pack()
-#         drop = OptionMenu(self.root, self.clicked, *self.options).pack()
-#         mybutton = Button(self.root, text='!בחרתי', command=self.change).pack()
-#
-#
-#     def change(self):
-#         """
-#         this is where i'm going to set the new options
-#         """
-#         sub_subs = ReadSubSubjects(self.hrefs, self.clicked.get())
-#         sub_subs.get_headers()
-#
-#         sub_menu = DisplaySubSubjects(sub_subs.options, self.root)
-#
-#
-# class ReadSubSubjects():
-#     """
-#     this class is for getting the headers of the page we have chosen to enter
-#     """
-#     def __init__(self, hrefs, clicked):
-#         self.hrefs = hrefs
-#         self.options = ['תבחר נושא']
-#         self.clicked = str(clicked)
-#
-#
-#
-# class DisplaySubSubjects(Display):
-#     def __init__(self, options, root):
-#         super().__init__(root)
-#         self.options = options
-#
-#         # build the interface
-#         self.drop = OptionMenu(self.root, self.clicked, *self.options).pack()
-#         mybutton = Button(self.root, text='submit', command=self.ask).pack()
-#
-#     def ask(self):
-#         """
-#         asking the user what he/she would like to know
-#         """
+            question = TheQuestion(self.menu.root)
+            question.show()
+            display_menus.append(question)
 
 
+class TheQuestion():
+    def __init__(self, root):
+        self.input = Entry(root)
+        self.dif_button = Button(root, text="?מה השאלה", command=self.go)
 
+    def show(self):
+        self.input.pack()
+        self.dif_button.pack()
+
+    def remove(self):
+        self.input.pack_forget()
+        self.dif_button.pack_forget()
+
+    def go(self):
+        print("fuck")
