209d5b45929c6b70530b957c1380c6dde903584e
Jonathan Yerushalmi <71553438+jonyeru23@users.noreply.github.com>
2021-03-14 23:26:36 +0200

    Add files via upload

---
+++ magic.py
@@ -0,0 +1,33 @@
+# """
+# this file is for all the actions that will take place from the question on
+# """
+# import requests
+# import PyPDF2
+# from bs4 import BeautifulSoup as bs
+#
+# class GetFiles():
+#     """
+#     this class is for making a dir with all files
+#     """
+#     def __init__(self, old_dict):
+#         self.old_dict = old_dict
+#         self.new_dict = {}
+#
+#     def make_new_dict(self):
+#         """
+#         returns a dictionary for each item for two links
+#         flip: ""
+#         real: ""
+#         type: ""
+#         http://online.fliphtml5.com/kfky/juwt/
+#         """
+#         flip = "fliphtml5"
+#         for name in self.old_dict:
+#             url = self.old_dict[name]
+#             html = None
+#             if flip in self.old_dict[name]:
+#                 # do stuff
+#
+#             a_dict['type'] = url[url.rfind('.')+1:]
+#             a_dict['real'] =
+#
