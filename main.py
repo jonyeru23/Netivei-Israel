d4a73f9089ba9003ac40aa715ebb286db032b9a0
Jonathan Yerushalmi <71553438+jonyeru23@users.noreply.github.com>
2021-03-17 22:52:50 +0200

    Update main.py
    i have found the answer!

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from helper import *
 import requests
 import PyPDF2
 import textract
+import codecs
+import re
 
 
 # THE MAIN URL
@@ -12,35 +14,33 @@ filter = 'מידע-לספקים'
 
 
 def main():
-    # # building my front page -- class
-    # page = FrontPage()
-    #
-    # # making a dict of options-links -- function
-    # main_subs_links = read_subjects(url_main, filter)
-    #
-    # # making the dropdown, label and a button -- class
-    # main_menu = DisplayMenu(page.root, ":מה הנושא המרכזי", main_subs_links.keys(), True,
-    #                         main_subs_links, url_main)
-    # display_menus.append(main_menu)
-    # main_menu.pack()
-    #
-    # page.root.mainloop()
+    # building my front page -- class
+    page = FrontPage()
+
+    # making a dict of options-links -- function
+    main_subs_links = read_subjects(url_main, filter)
+
+    # making the dropdown, label and a button -- class
+    main_menu = DisplayMenu(page.root, ":מה הנושא המרכזי", main_subs_links.keys(), True,
+                            main_subs_links, url_main)
+    display_menus.append(main_menu)
+    main_menu.pack()
+
+    page.root.mainloop()
 
     """ checking things"""
-    # file_url = "https://www.iroads.co.il/media/9771/%D7%A8%D7%99%D7%9B%D7%95%D7%96-%D7%93%D7%A4%D7%99-%D7%A2%D7%93%D7%9B%D7%95%D7%9F-%D7%9E%D7%A1-1-%D7%9C%D7%A4%D7%A8%D7%A7-0002.pdf"
+    # # access the right link
     #
-    # r = requests.get(file_url, stream=True)
+    # # parse the htmkl using bs
+    # # get to this link
+    # file_url = "http://online.fliphtml5.com/kfky/pzdc/files/search/book_config.js?bfe6ba3d2a8d19569d672697cccad432"
+    # r = requests.get(file_url)
+    # # print(r.encoding)
+    # soup = bs(r.text, features="lxml")
+    # text = soup.text.encode("latin1").decode("utf-8")
+    # text = re.sub("\s\s+", " ", text)
     #
-    # with open("python.pdf", "wb") as pdf:
-    #     for chunk in r.iter_content(chunk_size=1024):
-    #
-    #         # writing one chunk at a time to pdf file
-    #         if chunk:
-    #             pdf.write(chunk)
-
-    text = textract.process("C:\Users\User\Desktop\progrmaing\personal projects\finding nemo\python.pdf")
-    print(text)
-
+    # print(text)
 
 if __name__ == '__main__':
     main()
