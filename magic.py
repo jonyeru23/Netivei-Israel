06321ddb76c714e1eb4c464471c7de686461cd3c
Jonathan Yerushalmi <71553438+jonyeru23@users.noreply.github.com>
2021-03-17 22:51:22 +0200

    Update magic.py
    soon the magic will happen

--- magic.py
+++ magic.py
@@ -1,33 +1,24 @@
-# """
-# this file is for all the actions that will take place from the question on
-# """
-# import requests
-# import PyPDF2
-# from bs4 import BeautifulSoup as bs
+"""
+this file is for all the actions that will take place from the question on
+"""
+import requests
+import PyPDF2
+from bs4 import BeautifulSoup as bs
+
+# class LoadText:
+#     """a class to load all the text to a dict"""
+#     def __init__(self, subject_links):
+#         # a dict of the links and the headers
+#         self.subject_links = subject_links
+#         # dict[subject] = text
+#         self.text = {}
 #
-# class GetFiles():
-#     """
-#     this class is for making a dir with all files
-#     """
-#     def __init__(self, old_dict):
-#         self.old_dict = old_dict
-#         self.new_dict = {}
-#
-#     def make_new_dict(self):
-#         """
-#         returns a dictionary for each item for two links
-#         flip: ""
-#         real: ""
-#         type: ""
-#         http://online.fliphtml5.com/kfky/juwt/
-#         """
-#         flip = "fliphtml5"
-#         for name in self.old_dict:
-#             url = self.old_dict[name]
-#             html = None
-#             if flip in self.old_dict[name]:
-#                 # do stuff
-#
-#             a_dict['type'] = url[url.rfind('.')+1:]
-#             a_dict['real'] =
-#
+#     def load(self):
+#         for link in
+
+
+
+"""
+opening files
+"""
+
