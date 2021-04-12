413b458d8f715f625ead101805ea0fadb75b717c
Jonathan Yerushalmi <71553438+jonyeru23@users.noreply.github.com>
2021-04-12 07:18:38 +0300

    Update magic.py

--- magic.py
+++ magic.py
@@ -1,9 +1,14 @@
 """
 this file is for all the actions that will take place from the question on
 """
-import requests
-from bs4 import BeautifulSoup as bs
 from helper import *
+import textract
+import shutil
+import concurrent.futures as cf
+from string import punctuation
+import nltk
+
+
 
 # types for numbers
 flip = 'fliphtml5'
@@ -26,33 +31,61 @@ class LoadText:
     """
     def __init__(self, subject_links):
         # a dict of the links and the headers
-        self.micro_subject_links = subject_links
+        self.__micro_subject_links = subject_links
+        # path to new_dir and making a new dir
+        self.__path = None
         # a dict for text_name[name] = text
         self.texts = {}
-        # path to new_dir and making a new dir
-        self.path = make_dir()
-        self.load_files()
 
-    def load_files(self):
+    def get_texts(self):
+        """this is the interface!"""
+        self.__path = self.__make_dir()
+        # loads the files to the dict
+        self.__load_files()
+        for i, k in self.texts.items():
+            print(i)
+            print(k)
+        # parsers the dict
+        self.__destroy_dir()
+
+        return self.texts
+
+    def __make_dir(self):
+        """
+        make a new dir and return the path to that dir
+        """
+        if self.__path is not None:
+            self.__destroy_dir()
+        # get the current dir
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+
+        # make a new dir
+        dir_name = "files"
+        path = os.path.join(dir_path, dir_name)
+        os.mkdir(path)
+
+        return path
+
+    def __load_files(self):
         with cf.ThreadPoolExecutor() as executor:
-            executor.map(self.all_the_work, self.micro_subject_links.items())
+            executor.map(self.__all_the_work, self.__micro_subject_links.items())
 
-    def all_the_work(self, item):
-        # print(item[1])
+    def __all_the_work(self, item):
         """responsible for, checking the type, uploading it to the dict"""
-        file_type = self.check_for_type(item[1])
+        file_type = self.__check_for_type(item[1])
         if file_type == any(bad_types):
             return
         else:
-            self.load_the_file(file_type, item)
+            self.__load_the_file(file_type, item)
 
-    def check_for_type(self, link):
+    @staticmethod
+    def __check_for_type(link):
         """this function will check for a certain link type of file"""
         for file_type in types:
             if file_type in link:
                 return file_type
 
-    def load_the_file(self, file_type, item):
+    def __load_the_file(self, file_type, item):
         if file_type == flip:
             """we dont need to download the file to the dir if it's a flip"""
             r = requests.get(item[1])
@@ -60,25 +93,55 @@ class LoadText:
             links = soup.findAll('script')
             page = requests.get(item[1] + links[5].get('src', None))
 
-            text = page.text.encode("latin1").decode("utf-8")
-            text = re.sub("\s\s+", " ", text)
+            self.texts[item[0]] = self.__get_text_from_(page)
+
+        else:
+            """download the file and then get the text"""
+            full_path = self.__write_file(item)
+            text = textract.process(full_path)
+            text = text.decode("utf-8")
 
             self.texts[item[0]] = text
 
-        elif file_type == pdf:
-            """download the file and then get the text"""
-            full_path = self.write_file(item)
+    @staticmethod
+    def __get_text_from_(page):
+        """getting the text and manipiulating it (because it's hebrew"""
+        text = page.text.encode("latin1").decode("utf-8")
+        text = re.sub("\s\s+", " ", text)
+        text = re.sub("\u200f", " ", text)
+        return text
 
-
-
-    def write_file(self, item):
+    def __write_file(self, item):
         """writing the file and returning a path to the file"""
         req = requests.get(item[1])
-        full_path = os.path.join(self.path, item[0])
+        full_path = os.path.join(self.__path, item[0])
         with open(full_path, "wb") as f:
             for chunk in req.iter_content(chunk_size=8192):
                 f.write(chunk)
         return full_path
+
+    def __destroy_dir(self):
+        try:
+            shutil.rmtree(self.__path)
+        except OSError as e:
+            print("Error: %s : %s" % (self.__path, e.strerror))
+
+class TFIDF():
+    """The father class"""
+    def _tokenize(self, document):
+        """make every string given a list of space or punctuation marks separated tokens"""
+        stopwords = self._get_stop_words_hebrew('hebrew.txt')
+        words = nltk.word_tokenize(document)
+        return [word for word in words if word not in punctuation and
+                word not in stopwords]
+
+    @staticmethod
+    def _get_stop_words_hebrew(filename):
+        """takes the words in the .txt and makes it a list"""
+        with open(filename, encoding="utf8") as f:
+            text = f.read()
+            stopwords = list(text.split('\n'))
+        return stopwords
 
 """
 muy importante
